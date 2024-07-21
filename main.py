a04eb809a21ade04d525773f42e3e98962d0053f
Mohit Ginglani <113568405+RexMohit12@users.noreply.github.com>
2024-07-21 18:10:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ app.include_router(html_pages.router)  # Include the router for HTML pages
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0")
